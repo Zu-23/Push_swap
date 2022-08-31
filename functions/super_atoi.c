133c82b2fd65aa83a53c1fc69c1927f57e9ce2d2
zu-23 <h.zakaria52@gmail.com>
2022-08-30 22:29:47 -0400

    finished

--- functions/super_atoi.c
+++ functions/super_atoi.c
@@ -13,15 +13,14 @@
 #include "../actions.h"
 #include "../functions.h"
 
-
-void     after_int(char c, t_var *var, int *i)
+void	after_int(char c, t_var *var, int *i)
 {
-    if (c == '-' || c == '+')
-    {
-        if (c == '-')
-            var -> sign *= -1;
-        *i += 1;
-    }
+	if (c == '-' || c == '+')
+	{
+		if (c == '-')
+			var -> sign *= -1;
+		*i += 1;
+	}
 }
 
 void	allspaces(char *str, int *i)
@@ -37,29 +36,29 @@ void	var_declare(t_var *var)
 	var->sign = 1;
 }
 
-void    super_atoi(long **ar, char *ptr, int count)
+void	super_atoi(long **ar, char *ptr, int count)
 {
-    int				i;
-    static int		k;
-	t_var var;
- 
+	int				i;
+	static int		k;
+	t_var			var;
+
 	var_declare(&var);
-    i = 0;
-    if (!*ar)
-        *ar = malloc(sizeof(long) * count);
-    while(ptr[i])
-    {
-        after_int(ptr[i], &var, &i);
-        while (ptr[i] >= '0' && ptr[i] <= '9')
-            var.ret = var.ret * 10 + (ptr[i++] - 48);
-        if (ptr[i] == ' ' && ft_isdigit(ptr[i - 1]))
-        {
-            ar[0][k++] = var.ret * var.sign;
-            var.sign = 1;
-            var.ret = 0;
-        }
+	i = 0;
+	if (!*ar)
+		*ar = malloc(sizeof(long) * count);
+	while (ptr[i])
+	{
+		after_int(ptr[i], &var, &i);
+		while (ptr[i] >= '0' && ptr[i] <= '9')
+			var.ret = var.ret * 10 + (ptr[i++] - 48);
+		if (ptr[i] == ' ' && ft_isdigit(ptr[i - 1]))
+		{
+			ar[0][k++] = var.ret * var.sign;
+			var.sign = 1;
+			var.ret = 0;
+		}
 		allspaces(ptr, &i);
-    }
+	}
 	if (ft_isdigit(ptr[i - 1]))
-    	ar[0][k++] = var.ret * var.sign;
+		ar[0][k++] = var.ret * var.sign;
 }
